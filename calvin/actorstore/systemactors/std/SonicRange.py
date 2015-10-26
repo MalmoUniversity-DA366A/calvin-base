af5bc2e21ef601a02017d9b0896a7cfbca22c0f6
pdNor <nordahl.d@gmail.com>
2015-10-26 23:31:52 +0100

    Added script, not yet working

--- calvin/actorstore/systemactors/std/SonicRange.py
+++ calvin/actorstore/systemactors/std/SonicRange.py
@@ -7,19 +7,28 @@ from calvin.actor.actor import Actor, ActionResult, manage, condition
 
 class SonicRange(Actor):
 	"""
-	Forward a token unchanged
+	forward a token unchanged
 	Inputs:
-		token : a token
-	Outputs
-		token : the same token
+	  token : a token
+	Outputs:
+	  token : the same token
 	"""
-	@manage(['dump'])
 
+	@manage(['dump'])
 	def init(self, dump = False):
 		self.dump = dump
 
 	def log(self,data):
 		print "%s<%s>: %s" % (self.__class__.__name__, self.id, data)
+
+
+    @condition(['token'], ['token'])
+    def donothing(self, input):
+        if self.dump : self.log(input)
+        return ActionResult(tokens_consumed=1, tokens_produced=1, production=( input, ))
+
+
+	action_priority = ( donothing, )
 
 
 
