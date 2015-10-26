e8e17b0a7918306335cc0032bcbba86ba7dc598f
pdNor <nordahl.d@gmail.com>
2015-10-26 22:24:46 +0100

    Added very simple sonicRange actor for calvin arduino

---
+++ calvin/actorstore/systemactors/std/SonicRange.py
@@ -0,0 +1,30 @@
+#Author: Daniel Nordahl 2015-10-27
+#
+#Calvin-Arduino rage actor. Reads input from a sonic range
+#sensor and send the data as a token to a std actor.
+
+from calvin.actor.actor import Actor, ActionResult, manage, condition
+
+class SonicRange(Actor):
+	"""
+	orward a token unchanged
+	Inputs:
+		token : a token
+	Outputs
+		token : the same token
+	"""
+	@manage(['dump'])
+
+	def init(self, dump = False):
+		self.dump = dump
+
+	def log(self,data):
+		print "%s<%s>: %s" % (self.__class__.__name__, self.id, data)
+
+
+
+
+
+
+
+
