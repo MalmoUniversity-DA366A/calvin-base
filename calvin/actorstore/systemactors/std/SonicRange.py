d670f6afe424ba2dfd35a3642793d98fb4f0b84c
pdNor <nordahl.d@gmail.com>
2015-10-27 13:14:34 +0100

    Added sonic range actor that can be used as template actor

--- calvin/actorstore/systemactors/std/SonicRange.py
+++ calvin/actorstore/systemactors/std/SonicRange.py
@@ -1,39 +1,61 @@
-#Author: Daniel Nordahl 2015-10-27
-#
-#Calvin-Arduino rage actor. Reads input from a sonic range
-#sensor and send the data as a token to a std actor.
+# -*- coding: utf-8 -*-
 
-from calvin.actor.actor import Actor, ActionResult, manage, condition
+# Copyright (c) 2015 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, ActionResult, manage, condition, guard
+
 
 class SonicRange(Actor):
-	"""
-	forward a token unchanged
-	Inputs:
-	  token : a token
-	Outputs:
-	  token : the same token
-	"""
+    """
+    Send predetermined data on output.
+    Outputs:
+      token : Some data
+    """
+    @manage(['data', 'n', 'dump'])
+    def init(self, data, n=1, dump=False):
+        self.data = data
+        self.n = n
+        self.dump = dump
 
-	@manage(['dump'])
-	def init(self, dump = False):
-		self.dump = dump
+    def log(self, data):
+        print "%s<%s>: %s" % (self.__class__.__name__, self.id, data)
 
-	def log(self,data):
-		print "%s<%s>: %s" % (self.__class__.__name__, self.id, data)
+    @condition([], ['token'])
+    @guard(lambda self: self.n > 0 or self.n == -1)
+    def send_it(self):
+        if self.n > 0:
+            self.n -= 1
+        if self.dump:
+            self.log(self.data)
+        return ActionResult(production=(self.data,))
 
+    action_priority = (send_it, )
 
-    @condition(['token'], ['token'])
-    def donothing(self, input):
-        if self.dump : self.log(input)
-        return ActionResult(tokens_consumed=1, tokens_produced=1, production=( input, ))
+    test_args = (42,)
+    test_kwargs = {"n": 3}
 
+    test_set = [
+        {
+            'in': {},
+            'out': {'token': [42]}
+        } for i in range(3)
+    ]
 
-	action_priority = ( donothing, )
-
-
-
-
-
-
-
-
+    test_set += [
+        {
+            'in': {},
+            'out': {'token': []}
+        }
+    ]
